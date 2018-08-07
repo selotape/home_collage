64a46d773abec27f2b99d4619f4f38ae2d50bd5c
selotape <RonVisbord@gmail.com>
2018-08-07 15:42:58 +0300

    support configurable width and height

--- do_the_thing.py
+++ do_the_thing.py
@@ -1,61 +1,73 @@
 import os
 import random
-from math import sqrt
 
 import cv2
 import numpy as np
+from tqdm import tqdm
 
-IMAGES_PATH = "C:/Users/Ron/Dropbox/Code/py/home_collage/sample_images"
+IMAGES_PATH = "C:/Users/Ron/temp/ayelet_images"
+WIDTH_COUNT, HEIGHT_COUNT = 4, 3
 
 
 def do_the_thing():
-    images = get_all_images()
-    images = resample_images(images)
+    image_paths = search_image_paths()
+    image_paths = resample_images(image_paths)
+    images = read_images(image_paths)
     images = crop_images(images)
+    images = scale_images(images)
     images = pad_images(images)
     new_img = assemble_images(images)
     store_image(new_img)
 
 
-def get_all_images():
-    return [cv2.imread(dir[0] + '/' + f_name) for dir in os.walk(IMAGES_PATH) for f_name in dir[2]]
+def search_image_paths():
+    return [dir[0] + '/' + f_name
+            for dir in os.walk(IMAGES_PATH)
+            for f_name in dir[2]]
+
+
+def read_images(image_paths):
+    print('Loading images...')
+    return [cv2.imread(img_path) for img_path in tqdm(image_paths)]
 
 
 def resample_images(images):
-    n = grid_size(len(images))
-    return random.sample(images, n ** 2)
+    return random.sample(images, WIDTH_COUNT * HEIGHT_COUNT)
 
 
 def crop_images(images):
+    print('Cropping images...')
     min_width = min(img.shape[0] for img in images)
     min_height = min(img.shape[1] for img in images)
     return [img[:min_width, :min_height] for img in images]
 
 
+def scale_images(images):
+    return images
+
+
 def pad_images(images):
-    return images  # TODO - implement!
+    return images
 
 
 def assemble_images(images):
-    num_images = len(images)
-    n = grid_size(num_images)
-    result = np.concatenate(images[:n], axis=1)
+    print('Assembling image...')
 
-    for i in range(1, n):
-        new_row = np.concatenate(images[i * n:(i + 1) * n], axis=1)
-        result = np.concatenate((result, new_row), axis=0)
+    first_row = np.concatenate(images[:WIDTH_COUNT], axis=1)
+    result = first_row
+    for i in range(1, HEIGHT_COUNT):
+        next_row = np.concatenate(images[i * WIDTH_COUNT:(i + 1) * WIDTH_COUNT], axis=1)
+        result = np.concatenate((result, next_row), axis=0)
     return result
 
 
 def store_image(new_img):
+    print('Storing image...')
+
     cv2.imshow('image', new_img)
     cv2.imwrite('result.jpg', new_img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-
-
-def grid_size(num_images):
-    return int(sqrt(num_images))
 
 
 if __name__ == '__main__':
