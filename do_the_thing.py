ca992e35292f43e81d6d915b7c084fdc58834430
selotape <RonVisbord@gmail.com>
2018-08-08 13:28:37 +0300

    first sweet functional version!

--- do_the_thing.py
+++ do_the_thing.py
@@ -3,27 +3,34 @@ import random
 from functools import partial
 
 import cv2
-from tqdm import tqdm
+import numpy as np
 
 ### CONF ###
-# IMAGES_PATH = "C:/Users/Ron/temp/ayelet_images"
-IMAGES_PATH = "C:/Users/Ron/Dropbox/Code/py/home_collage/sample_images"
+IMAGES_PATH = "C:/Users/Ron/temp/ayelet_images"
+# IMAGES_PATH = "C:/Users/Ron/Dropbox/Code/py/home_collage/sample_images"
 OUTPUT_PATH = "C:/Users/Ron/temp/result.jpg"
-WIDTH_IN_PIXELS, HEIGHT_IN_PIXELS = 6780, 3012
-IMG_HEIGHT_IN_PIXELS = 140
-INNER_PAD_IN_PIXELS, OUTER_PAD_IN_PIXELS = 2, 5
+WIDTH_IN_PIXELS, HEIGHT_IN_PIXELS = 4000, 2500
+IMG_HEIGHT_IN_PIXELS = 120
+IMAGE_PAD_IN_PIXELS, COLLAGE_PAD_IN_PIXELS = 2, 5
 
 ### CONSTS ###
 WHITE = [255, 255, 255]
 
 
+def print_metrics():
+    print('Expected num rows: %d' % (HEIGHT_IN_PIXELS // IMG_HEIGHT_IN_PIXELS))
+    print('Expected imgs per rows: %d' % (WIDTH_IN_PIXELS // IMG_HEIGHT_IN_PIXELS))
+    print('Expected total images: %d' % (HEIGHT_IN_PIXELS * WIDTH_IN_PIXELS // IMG_HEIGHT_IN_PIXELS ** 2))
+
+
 def do_the_thing():
+    print_metrics()
     image_paths = search_image_paths()
     images = read_images(image_paths)
     images = scale_images(images)
     images = pad_images(images)
-    new_img = assemble_image(images)
-    new_img = pad_image(new_img)
+    new_img = assemble_collage(images)
+    new_img = pad_collage(new_img)
     store_image(new_img)
 
 
@@ -34,15 +41,20 @@ def search_image_paths():
 
 
 def read_images(image_paths):
-    return (cv2.imread(img_path) for img_path in tqdm(image_paths))
+    for i, img_path in enumerate(image_paths, start=1):
+        print("Reading %dth image ('%s')..." % (i, img_path))
+        yield cv2.imread(img_path)
 
 
-def pad_image(new_img):
-    return new_img
+def pad_collage(collage):
+    return cv2.copyMakeBorder(collage,
+                              top=IMAGE_PAD_IN_PIXELS, bottom=IMAGE_PAD_IN_PIXELS, left=IMAGE_PAD_IN_PIXELS, right=IMAGE_PAD_IN_PIXELS,
+                              borderType=cv2.BORDER_CONSTANT, value=WHITE)
 
 
 def scale_images(images):
-    for image in images:
+    for i, image in enumerate(images, start=1):
+        print("Scaling %dth image..." % i)
         yield scale_image(image)
 
 
@@ -62,21 +74,62 @@ def width(img):
 
 def pad_images(images):
     pad = partial(cv2.copyMakeBorder,
-                  top=INNER_PAD_IN_PIXELS, bottom=INNER_PAD_IN_PIXELS, left=INNER_PAD_IN_PIXELS,
-                  right=INNER_PAD_IN_PIXELS,
+                  top=IMAGE_PAD_IN_PIXELS, bottom=IMAGE_PAD_IN_PIXELS, left=IMAGE_PAD_IN_PIXELS,
+                  right=IMAGE_PAD_IN_PIXELS,
                   borderType=cv2.BORDER_CONSTANT, value=WHITE)
     return (pad(img) for img in images)
 
 
-def assemble_image(images):
-    # print('Assembling image...')
-    #
-    # first_row = np.concatenate(images[:WIDTH_COUNT], axis=1)
-    # result = first_row
-    # for i in range(1, HEIGHT_COUNT):
-    #     next_row = np.concatenate(images[i * WIDTH_COUNT:(i + 1) * WIDTH_COUNT], axis=1)
-    #     result = np.concatenate((result, next_row), axis=0)
-    return next(images)
+def assemble_collage(images):
+    images = iter(images)
+    first_row = assemble_row(images)
+    collage = first_row
+    num_rows = HEIGHT_IN_PIXELS // IMG_HEIGHT_IN_PIXELS
+    for _ in range(num_rows - 1):
+        next_row = assemble_row(images)
+        collage = np.concatenate((collage, next_row), axis=0)
+    return collage
+
+
+def assemble_row(images):
+    row_images = choose_row_images(images)
+    print('Images in row: %d' % len(row_images))
+    complete_missing_columns(row_images)
+    return np.concatenate(row_images, axis=1)
+
+
+def choose_row_images(images):
+    row_images = []
+    next_img = next(images)
+    while sum(width(img) for img in row_images) + width(next_img) <= WIDTH_IN_PIXELS:
+        row_images.append(next_img)
+        next_img = next(images)
+    return row_images
+
+
+def complete_missing_columns(row_images):
+    # type: (list) -> None
+    missing_columns = WIDTH_IN_PIXELS - sum(width(img) for img in row_images)
+    num_images = len(row_images)
+    while missing_columns >= num_images - 1:
+        for i in range(num_images):
+            right_pad_row_image(i, row_images)
+            missing_columns -= 1
+
+    while missing_columns > 0:
+        rand_ix = random.randrange(num_images - 1)
+        right_pad_row_image(rand_ix, row_images)
+        missing_columns -= 1
+
+    final_row_width = sum(width(img) for img in row_images)
+    assert final_row_width == WIDTH_IN_PIXELS, "Row doesn't reach target width. target:%d. actual:%d" % (WIDTH_IN_PIXELS, final_row_width)
+
+
+def right_pad_row_image(img_ix, row_images):
+    img = row_images.pop(img_ix)
+    img = cv2.copyMakeBorder(src=img, top=0, bottom=0, left=0, right=1,
+                             borderType=cv2.BORDER_CONSTANT, value=WHITE)
+    row_images.insert(img_ix, img)
 
 
 def store_image(new_img):
